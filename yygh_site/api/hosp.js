c3ce4c0cd8dc4e5de4fbc29e2d2ab24d58a7dd1a
wy <1421329428@qq.com>
2024-04-18 22:31:25 +0800

    前台首页页面搭建

---
+++ yygh_site/api/hosp.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+const api_name = `/api/hosp/hospital`
+
+export default {
+ getPageList(page, limit, searchObj) {
+    return request({
+        url: `${api_name}/findHospList/${page}/${limit}`,
+        method: 'get',
+        params: searchObj
+    })
+  },
+
+ getByHosname(hosname) {
+    return request({
+        url: `${api_name}/findByHosName/${hosname}`,
+        method: 'get'
+    })
+  },
+
+  show(hoscode) {
+    return request({
+        url: `${api_name}/findHospDetail/${hoscode}`,
+        method: 'get'
+    })
+  },
+  findDepartment(hoscode) {
+      return request({
+          url: `${api_name}/department/${hoscode}`,
+          method: 'get'
+      })
+  }
+}
